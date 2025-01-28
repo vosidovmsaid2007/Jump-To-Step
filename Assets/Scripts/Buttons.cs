2dcb84f4181f361f22f834cb9da6eb9ae19e1178
vosidovmsaid2007 <msaidvosidov@gmail.com>
2025-01-28 18:28:41 +0500

    Added animation to buttons

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        transform.localScale = new Vector3(1.05f, 1.05f, 1.05f);
+    }
+
+    void OnMouseUp()
+    {
+        transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+
+}
---
+++ Assets/Scripts/TextFade.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFade : MonoBehaviour
+{
+    private Text txt;
+    private Outline oLine;
+
+    void Start(){
+        txt = GetComponent<Text>();
+        oLine = GetComponent<Outline>();
+    }
+
+    void Update(){
+        txt.color = new Color(txt.color.r, txt.color.g, txt.color.b, Mathf.PingPong(Time.time / 2.5f, 1.0f));
+        oLine.effectColor = new Color(oLine.effectColor.r, oLine.effectColor.g, oLine.effectColor.b, txt.color.a - 0.3f);
+    }
+}
