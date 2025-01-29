445d1b5532506c816fae148dd3544a2b1d2f4258
vosidovmsaid2007 <msaidvosidov@gmail.com>
2025-01-29 14:30:02 +0500

    Design Main Scene

---
+++ Assets/Scripts/SpawnStars.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStars : MonoBehaviour
+{
+    public GameObject star;
+
+    void Start()
+    {
+        StartCoroutine(spawn());
+    }
+
+    IEnumerator spawn()
+    {
+        while(true)
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), Camera.main.farClipPlane / 2));
+            Instantiate(star, pos, Quaternion.Euler(0, 0, Random.Range(0f, 360f)));
+            yield return new WaitForSeconds(5.01f);
+        }
+    }    
+}
---
+++ Assets/Scripts/StarAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarAnimation : MonoBehaviour
+{
+    private SpriteRenderer star;
+    private float movementSpeed = 0.1f;
+
+    void Start()
+    {
+        star = GetComponent<SpriteRenderer>();
+        Destroy(gameObject, 5f);
+    }
+
+    void Update()
+    {
+        star.color = new Color(star.color.r, star.color.g, star.color.b, Mathf.PingPong(Time.time / 2.5f, 1.0f));
+    
+        transform.position += transform.up * Time.deltaTime * movementSpeed;
+    }
+}
