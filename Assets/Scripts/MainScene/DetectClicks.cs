5a5bfb77ca1ebe741c0e136cb7ccb2f0c24fda33
vosidovmsaid2007 <msaidvosidov@gmail.com>
2025-01-29 21:07:07 +0500

    Start the game

---
+++ Assets/Scripts/MainScene/DetectClicks.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectClicks : MonoBehaviour
+{
+    public GameObject buttons, m_cube;
+    public Text playTxt, gameName;
+
+    private bool clicked;
+
+    void OnMouseDown()
+    {
+        if(!clicked){
+            playTxt.gameObject.SetActive(false);
+            gameName.text = "0";
+            buttons.GetComponent<ScrollObjects>().speed = -5f;
+            buttons.GetComponent<ScrollObjects>().checkPos = -150f;
+            m_cube.GetComponent<Animation>().Play("StartGameCube");
+        }  
+    }
+}
